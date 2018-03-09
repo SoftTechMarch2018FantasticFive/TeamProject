cc59d01fc4e51e06fbb69f2b2f52a17d69df5076
NinoBonev <nbonev@gmail.com>
2018-03-09 11:13:07 +0000

    Created CharacterStars/Program.cs file

---
+++ 05. Character Stats/CharacterStars/CharacterStars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CharacterStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
