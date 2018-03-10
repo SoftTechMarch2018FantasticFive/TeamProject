c5e091035e5c8884ab6722bb3ed32e8918ea8f6b
denishristov <plaritus@gmail.com>
2018-03-10 14:39:57 +0200

    Added 03 and 04 and cleaned up the repo

---
+++ C# Intro and Basic Syntax - Exercises/03.MilesToKilometres/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Miles_to_Kilometres
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
