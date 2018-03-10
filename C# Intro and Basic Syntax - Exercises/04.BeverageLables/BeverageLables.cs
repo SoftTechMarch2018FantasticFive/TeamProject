5d813ee8a6b1fe96bcc6aa9b2d38c4ad028cbd9a
denishristov <plaritus@gmail.com>
2018-03-10 20:02:53 +0200

    Update BeverageLables.cs

--- C# Intro and Basic Syntax - Exercises/04.BeverageLables/BeverageLables.cs
+++ C# Intro and Basic Syntax - Exercises/04.BeverageLables/BeverageLables.cs
@@ -1,4 +1,5 @@
-﻿using System;
+// CONFIRMED from <denishristov>
+using System;
 using System.Collections.Generic;
 
 namespace BeverageLables
@@ -7,17 +8,22 @@ namespace BeverageLables
     {
         public static void Main(string[] args)
         {
+            // Get the name of the beverage from the input 
             var name = Console.ReadLine();
 
+            // Initialize a list of the beverage's properties
             var properties = new List<double>();
 
             for (int i = 0; i < 3; i++)
             {
+                // Parse and add the input data to the list
                 properties.Add(double.Parse(Console.ReadLine()));  
             }
 
+            // Print out the name and volume of the beverage
             Console.WriteLine($"{properties[0]}ml {name}:");
 
+            // Print out it's calories and sugar
             Console.WriteLine($"{properties[1] * properties[0] / 100}kcal, {properties[2] * properties[0] / 100}g sugars");
         }
     }
