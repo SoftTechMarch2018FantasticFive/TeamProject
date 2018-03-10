98172add6ecba8b816ad84be014a0f1d00240490
denishristov <plaritus@gmail.com>
2018-03-10 14:53:21 +0200

    Added 01 and gitignore

---
+++ C# Intro and Basic Syntax - Exercises/01.DebitCardNumber/Debit CardNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DebitCardNumber
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var card = new List<int>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                card.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine($"{card[0]:0000} {card[1]:0000} {card[2]:0000} {card[3]:0000}");
+        }
+    }
+}
