760535da00a09c38c14b485e267c1fa5313b56b9
denishristov <plaritus@gmail.com>
2018-03-10 20:06:34 +0200

    Update CharacterStars.cs

--- C# Intro and Basic Syntax - Exercises/05.CharacterStats/CharacterStars.cs
+++ C# Intro and Basic Syntax - Exercises/05.CharacterStats/CharacterStars.cs
@@ -1,4 +1,5 @@
-﻿using System;
+// CONFIRMED from <denishristov>
+using System;
 
 namespace CharacterStars
 {
@@ -6,13 +7,17 @@ namespace CharacterStars
     {
         static void Main(string[] args)
         {
+            // Get and parse the input data
             string name = Console.ReadLine();
             int currentHealth = int.Parse(Console.ReadLine());
             int maxHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maxEnergy = int.Parse(Console.ReadLine());
 
+            // Print out the name
             Console.WriteLine($"Name: {name}");
+            
+            // Create the Health and Energy bars and print them out
             Console.WriteLine($"Health: |{new string('|', currentHealth)}{new string('.', maxHealth - currentHealth)}|");
             Console.WriteLine($"Energy: |{new string('|', currentEnergy)}{new string('.', maxEnergy - currentEnergy)}|");
             
