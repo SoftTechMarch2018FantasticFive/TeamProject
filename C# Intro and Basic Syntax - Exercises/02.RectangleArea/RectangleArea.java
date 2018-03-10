eef06441bd9c85459ea1e11e790c25dc42862dfe
denishristov <plaritus@gmail.com>
2018-03-10 19:58:29 +0200

    Update RectangleArea.java

--- C# Intro and Basic Syntax - Exercises/02.RectangleArea/RectangleArea.java
+++ C# Intro and Basic Syntax - Exercises/02.RectangleArea/RectangleArea.java
@@ -1,17 +1,21 @@
+// CONFIRMED from <denishristov>
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class RectangleArea {
-    public static void main(String[] args) throws IOException{
-
+    public static void main(String[] args) throws IOException {
+        // Initialize a console reader
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
+        // Parse the input data 
         double rectangleWidth = Double.parseDouble(console.readLine());
         double rectangleHeight = Double.parseDouble(console.readLine());
 
+        // Calculate the answer
         double rectangleArea = rectangleHeight * rectangleWidth;
 
+        // Print out the answer rounded to the 2nd digit after the decimal point
         System.out.printf("%.2f", rectangleArea);
     }
 }
