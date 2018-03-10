034fd3331bbf7dfd4b87f827b9b02b9cfb2b2f2f
Bastianmark <32564775+Bastianmark@users.noreply.github.com>
2018-03-10 17:35:40 +0200

    Update Debit Card Number
    Added step by step DESCRIPTION.
    Added // CONFIRMATION.

--- C# Intro and Basic Syntax - Exercises/01.DebitCardNumber/DebitCardNumber.cs
+++ C# Intro and Basic Syntax - Exercises/01.DebitCardNumber/DebitCardNumber.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+// CONFIRMED <BASTIANMARK>
 
 namespace DebitCardNumber
 {
@@ -7,13 +8,17 @@ namespace DebitCardNumber
     {
         public static void Main(string[] args)
         {
+            // 4 numbers are received from the Console on separate lines.
             var card = new List<int>();
+            // Create a new LIST of numbers.
 
             for (int i = 0; i < 4; i++)
             {
                 card.Add(int.Parse(Console.ReadLine()));
+                //Reads the numbers from the CONSOLE and FILLS the LIST
             }
             Console.WriteLine($"{card[0]:0000} {card[1]:0000} {card[2]:0000} {card[3]:0000}");
+            // PRINTs the LIST of numbers stored in the format 0000. 
         }
     }
 }
