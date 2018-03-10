f39e343872b93068eeb41d0e07b3f69746a90ee9
NinoBonev <nbonev@gmail.com>
2018-03-10 09:44:49 +0000

    Solved Rectangle Area Task

--- 02. Rectangle Area/src/RectangleArea.java
+++ 02. Rectangle Area/src/RectangleArea.java
@@ -1,6 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class RectangleArea {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
+        double rectangleWidth = Double.parseDouble(console.readLine());
+        double rectangleHeight = Double.parseDouble(console.readLine());
+
+        double rectangleArea = rectangleHeight * rectangleWidth;
+
+        System.out.printf("%.2f", rectangleArea);
     }
 }
